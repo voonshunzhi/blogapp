b7e4bff3484ec287f426e61db58edfc84e350dc2
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-07-23 10:22:43 +0000

    Style devise sign up views template

--- spec/features/signing_ups_spec.rb
+++ spec/features/signing_ups_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "SigningUps", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "with valid credentials" do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'Email', with:"user@example.com"
+    fill_in 'Password',with:'password'
+    fill_in 'Password confirmation',with:'password'
+    click_button 'Sign up'
+    
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+  
+  scenario "with invalid credentials" do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'Email', with:""
+    fill_in 'Password',with:""
+    fill_in 'Password confirmation',with:""
+    click_button 'Sign up'
+    
+    #expect(page).to have_content("You have not signed up successfully.")
+  end
 end

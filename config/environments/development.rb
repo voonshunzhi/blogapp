34b6cb8ae9c0f1f50379ef4f6e10f7c380545072
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-07-23 01:31:21 +0000

    Devise setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  #Setting the default url of you apps
+  config.action_mailer.default_url_options = { host: "https://blogapp-voonshunzhi.c9users.io"}
 end

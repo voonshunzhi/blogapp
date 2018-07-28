b3d046ebc34ad0155946743c263bb614a127cbdd
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-07-28 17:48:48 +0000

    Done action-cable

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,9 +1,11 @@
+#The first block set the user id cookies 
 Warden::Manager.after_set_user do |user, auth, opts| 
-scope = opts[:scope] 
-auth.cookies.signed["#{scope}.id"] = user.id
+    scope = opts[:scope] 
+    auth.cookies.signed["#{scope}.id"] = user.id
 end
 
+#This block clear the cookies when the user sign out because without this only session is cleared
 Warden::Manager.before_logout do |user, auth, opts| 
-scope = opts[:scope] 
-auth.cookies.signed["#{scope}.id"] = nil
+    scope = opts[:scope] 
+    auth.cookies.signed["#{scope}.id"] = nil
 end
